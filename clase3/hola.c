aa4da1d36ca971026c3726f93f147ebc86783368
Mauricio Quijada <majiqtresi@gmail.com>
2016-11-11 17:39:58 -0600

    Agregando mas funciones mpi a hola.c *basicas*

--- clase3/hola.c
+++ clase3/hola.c
@@ -3,11 +3,16 @@
 
 
 main ( int arc, char *argv[]){
-  int id, np;  
+  int id, np, v, sv, len;
+  char nombre[20];  
 MPI_Init(&arc, &argv);
    
    MPI_Comm_rank(MPI_COMM_WORLD, &id);
    MPI_Comm_size(MPI_COMM_WORLD, &np);
    printf("hola desde el proceso %d de %d\n", id, np);
+   MPI_Get_version(&v, &sv);
+   MPI_Get_processor_name(nombre, &len);
+   printf("La version es %d . %d", v ,sv);
+   printf("El nombre del host es %s\n", nombre);
 MPI_Finalize();
 }

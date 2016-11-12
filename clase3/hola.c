7da7d17a452dd3ec43562e95f710733e70b5a2ed
Mauricio Quijada <majiqtresi@gmail.com>
2016-11-11 18:25:19 -0600

    Add more changes to hola.c

--- clase3/hola.c
+++ clase3/hola.c
@@ -4,7 +4,7 @@
 
 int main ( int arc, char *argv[]){
   
-int id, np, dato=0; 
+int id, np, dato[10],i; 
 MPI_Status estado;
 MPI_Init(&arc, &argv);
    
@@ -16,20 +16,26 @@ MPI_Init(&arc, &argv);
       return 1;
    }
    if (id==0){
-      dato = 7;
-      MPI_Send(&dato, 1,MPI_INT,1,11, MPI_COMM_WORLD);
-      MPI_Recv(&dato, 1,MPI_INT,1,12, MPI_COMM_WORLD, &estado);
+      //llenaArreglo(dato);
+      for (i = 0; i <10 ; i++)
+         dato[i]=i;
+      MPI_Send(&dato, 10,MPI_INT,1,11, MPI_COMM_WORLD);
+      MPI_Recv(&dato, 10,MPI_INT,1,12, MPI_COMM_WORLD, &estado);
       printf("dato=%d\n",dato);
    }
    else{
       //
       MPI_Recv(&dato, 1,MPI_INT,0,11, MPI_COMM_WORLD, &estado);
-      dato++;
+      //dato++;
       MPI_Send (&dato,1,MPI_INT,0,12,MPI_COMM_WORLD); 
+     //imprimeArreglo(dato);
      //printf("El dato es %d\n", dato);
-      
+     for (i = 0; i <10 ; i++)
+        dato[i]=i; 
    }
 MPI_Finalize();
 return 0;
 
 }//fin de main
+
+
